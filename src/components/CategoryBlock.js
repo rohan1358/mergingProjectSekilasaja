7edc084abc7ccd973c5110cceb40bc80321aa796
Joy Lodra <lodrajoy@gmail.com>
2021-07-27 00:25:58 -0700

    Finished home page, but havent done category filter bar, lets make a new trello card for that

--- src/components/CategoryBlock.js
+++ src/components/CategoryBlock.js
@@ -31,75 +31,61 @@ export default function CategoryBlock(props) {
       </div>
 
       <div>
-        <CategoryBar
-          color="secondary"
-          tabs={[
-            {
-              tabButton: "Profile",
-              tabContent: (
-                <span>
-                  <p>
-                    Collaboratively administrate empowered markets via
-                    plug-and-play networks. Dynamically procrastinate B2C users
-                    after installed base benefits.
-                  </p>
-                  <br />
-                  <p>
-                    Dramatically visualize customer directed convergence without
-                    revolutionary ROI. Collaboratively administrate empowered
-                    markets via plug-and-play networks. Dynamically
-                    procrastinate B2C users after installed base benefits.
-                  </p>
-                  <br />
-                  <p>This is very nice.</p>
-                </span>
-              ),
-            },
-            {
-              tabButton: "Settings",
-              tabContent: (
-                <span>
-                  <p>
-                    Efficiently unleash cross-media information without
-                    cross-media value. Quickly maximize timely deliverables for
-                    real-time schemas.
-                  </p>
-                  <br />
-                  <p>
-                    Dramatically maintain clicks-and-mortar solutions without
-                    functional solutions.
-                  </p>
-                </span>
-              ),
-            },
-            {
-              tabButton: "Options",
-              tabContent: (
-                <span>
-                  <p>
-                    Completely synergize resource taxing relationships via
-                    premier niche markets. Professionally cultivate one-to-one
-                    customer service with robust ideas.{" "}
-                  </p>
-                  <br />
-                  <p>
-                    Dynamically innovate resource-leveling customer service for
-                    state of the art customer service.
-                  </p>
-                </span>
-              ),
-            },
-          ]}
-        />
-        <Grid container spacing={3}>
-          <BookCard
-            cover={BookCover}
-            title={"Rich Dad Poor Dad"}
-            author={"Robert Kiyosaki"}
-            description={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
-            }
-          />
+        <Grid container spacing={7}>
+          <Grid item>
+            <BookCard
+              cover={BookCover}
+              title={"Rich Dad Poor Dad"}
+              author={"Robert Kiyosaki"}
+              description={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
+              }
+            />
+          </Grid>
+
+          <Grid item>
+            <BookCard
+              cover={BookCover}
+              title={"Rich Dad Poor Dad"}
+              author={"Robert Kiyosaki"}
+              description={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
+              }
+            />
+          </Grid>
+
+          <Grid item>
+            <BookCard
+              cover={BookCover}
+              title={"Rich Dad Poor Dad"}
+              author={"Robert Kiyosaki"}
+              description={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
+              }
+            />
+          </Grid>
+
+          <Grid item>
+            <BookCard
+              cover={BookCover}
+              title={"Rich Dad Poor Dad"}
+              author={"Robert Kiyosaki"}
+              description={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
+              }
+            />
+          </Grid>
+
+          <Grid item>
+            <BookCard
+              cover={BookCover}
+              title={"Rich Dad Poor Dad"}
+              author={"Robert Kiyosaki"}
+              description={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
+              }
+            />
+          </Grid>
         </Grid>
       </div>
     </div>
