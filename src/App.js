0e28a2b5912df4c359733481671f0cc9b5f0c953
Joy Lodra <lodrajoy@gmail.com>
2021-08-06 17:42:26 -0700

    started creating accounts page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import FourOFourPage from "./pages/404page";
 import PricingPage from "./pages/Pricing";
 import BookDetailsPage from "./pages/BookDetailsPage";
 import VideoWatchingPage from "./pages/VideoWatching";
+import AccountsPage from "./pages/Accounts";
 import NewWebLanding from "./pages/NewWebLanding";
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Route exact path="/pricing" component={PricingPage} />
           <Route exact path="/book-details" component={BookDetailsPage} />
           <Route exact path="/video-page" component={VideoWatchingPage} />
+          <Route exact path="/accounts-page" component={AccountsPage} />
           <Route exact path="/" component={Home} />
           <Route component={FourOFourPage} />
           {/* <Route component={NewWebLanding} /> */}
--- src/pages/Accounts.js
+++ src/pages/Accounts.js
@@ -1,1 +1,20 @@
+import React from "react";
 
+// Custom components
+import Parallax from "../components/Parallax";
+import Typography from "../components/Typography";
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+// Material-UI components
+import { Container } from "@material-ui/core";
+
+export default function AccountsPage() {
+  return (
+    <div>
+      <NavBar />
+      <Container>Accounts Page</Container>
+      <Footer />
+    </div>
+  );
+}
