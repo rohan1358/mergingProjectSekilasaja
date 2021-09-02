4846e0ad40093e5a0f28079f0587db8fda693758
Gerry Oei <gerryoei.10.1.iics@gmail.com>
2021-09-02 15:01:52 -0400

    Fixed issue with error messages in payment and added functionality to empty cart on finishing payment

--- src/pages/Payment/PaymentSuccess.js
+++ src/pages/Payment/PaymentSuccess.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 
 // Custom components
 import Typography from "../../components/Typography";
@@ -9,7 +9,41 @@ import Button from "../../components/Button";
 import { Container, Grid } from "@material-ui/core";
 import HomeIcon from "@material-ui/icons/Home";
 
+import { AuthContext } from "../../components/Routing/Auth";
+
+//Redux
+import { useSelector, useDispatch } from "react-redux";
+import { selectCart, setCart } from "../../feature/cartSlice";
+import { selectUser } from "../../feature/userSlice";
+
+//Firebase
+import fire from "../../firebase/fire";
+
 export default function PaymentSuccess() {
+  const dispatch = useDispatch();
+  const firestore = fire.firestore();
+  const { currentUser } = useContext(AuthContext);
+  const userData = useSelector(selectUser);
+  const cart = useSelector(selectCart);
+
+  useEffect(() => {
+    const emptyCart = async () => {
+      if(currentUser && cart.cart.length > 0){
+        firestore.collection("users").doc(currentUser.uid).update({
+          cart: [],
+        }).then(function() {
+          dispatch(setCart([]));
+          console.log("Emptied cart...");
+        }).catch((err) => {
+          var errorCode = err.code;
+          var errorMessage = err.message;
+          console.log("Error: " + errorCode + "\n\n" + errorMessage);
+        });
+      }
+    };
+    emptyCart();
+  }, []);
+  
   const multi = MultiUseMobile();
   return (
     <div>
