e12f13f2141ff85927669d873ce8428f09b6fa0a
Joy Lodra <lodrajoy@gmail.com>
2021-09-17 19:04:22 -0700

    a

--- src/pages/SignUp/SignUpForm.js
+++ src/pages/SignUp/SignUpForm.js
@@ -94,6 +94,7 @@ const SignUpForm = ({ history }) => {
               cart: [],
               start_date: new Date("9/9/99"), // this date means UNSUBSCRIBED
               end_date: new Date("9/9/99"), // this date means UNSUBSCRIBED
+              sign_up_date: new Date(),
             })
             .then((resp) => {
               console.log("Added user data to firestore...");
